874f0866232deb1849d874b3a495a7b0758b7d5b
jpviegas <jp.viegas@hotmail.com>
2022-12-01 16:09:17 -0300

    vercel 4

--- src/index.js
+++ src/index.js
@@ -1,39 +1,38 @@
 import {config} from 'dotenv'
 import express from 'express'
 import cors from 'cors'
-// import mongoose from 'mongoose'
+import mongoose from 'mongoose'
 config()
 
-// import Todo from "./models/Todo.js";
+import Todo from "./models/Todo.js";
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 const port = 3000
-// const MONGO_URL = process.env.MONGO_URL
+const MONGO_URL = process.env.MONGO_URL
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-// app.get('/', async (_req,res)=>{
-//   const todos = await Todo.find();
-//   res.json(todos);
-// })
-
-app.get('/', async (_req,res)=>{
-  // const todos = await Todo.find();
-  res.json({
-    "message": "nodejs api"
-  });
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
 
-// mongoose.connect(MONGO_URL ?? '',
-// ).then(() => {
-//   console.log(`on port ${port}`);
-//   app.listen(process.env.PORT || port);
-// });
+app.get('/todos', async (_req,res)=>{
+  const todos = await Todo.find();
+  res.json(todos);
+})
 
-app.listen(process.env.PORT || port,()=>{
-  console.log(`port ${port}`);
+// app.get('/', async (_req,res)=>{
+//   res.json({
+//     message: "nodejs api"
+//   });
+// })
+
+mongoose.connect(MONGO_URL ?? '',
+).then(() => {
+  console.log(`on port ${port}`);
+  app.listen(process.env.PORT || port);
 });
+
+// app.listen(process.env.PORT || port,()=>{
+//   console.log(`port ${port}`);
+// });
