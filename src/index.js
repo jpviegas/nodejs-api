84f453e7c2d4ae8d8ccc1a5ae920ce6a0dbc92e5
jpviegas <jp.viegas@hotmail.com>
2022-12-01 14:19:40 -0300

    port

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import {config} from 'dotenv'
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
-// import getTodosController from './controllers/getTodosController'
 config()
 
 import Todo from "./models/Todo.js";
@@ -13,19 +12,17 @@ app.use(express.json())
 const port = 3000
 const MONGO_URL = process.env.MONGO_URL
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
 
-app.get('/todos', async (_req,res)=>{
+app.get('/', async (_req,res)=>{
   const todos = await Todo.find();
   res.json(todos);
 })
 
-// app.get('/todos', getTodosController);
-
 mongoose.connect(MONGO_URL ?? '',
 ).then(() => {
   console.log(`on port ${port}`);
-  app.listen(process.env.PORT);
+  app.listen(process.env.PORT || port);
 });
